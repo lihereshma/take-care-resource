929a2a23d5afef1fbd9f621fa675d186179dcb03
lreshmaprdxn <reshmal@prdxn.com>
2020-03-18 14:00:36 +0530

    nav function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+// Function for navigation sidebar
+var hamburger = document.querySelector ('.hamburger');
+var nav = document.querySelector ('.menu');
+hamburger.addEventListener ('click',showNav);
 
+function showNav() {
+  nav.classList.toggle('show');  
+  hamburger.classList.toggle('open');
+  document.querySelector('html').classList.toggle('no-scroll');
+}
 
 
 
